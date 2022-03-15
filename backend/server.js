1c84b538f22e5bb1b32b2cd3a9db608515aa73a7
mahesh <banagarmahesh2002@gmail.com>
2022-03-15 14:44:27 +0530

    redeploy

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,8 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-if (process.env.NODE_ENV !== "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    );
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running....");
-    });
-}
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -39,8 +31,16 @@ app.get("/api/config/paypal", (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running....");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
