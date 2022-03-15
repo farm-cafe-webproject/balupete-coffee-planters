23055d0cc935d62481efa18aaa5f2d3096a6cd70
mahesh <banagarmahesh2002@gmail.com>
2022-03-15 14:06:05 +0530

    d3

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
     app.get("*", (req, res) =>
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
